d7b09d22e935290af1cc91635fc06a1e4a706b7d
Pedro Nascimento <nennogabriel@gmail.com>
2019-10-19 08:55:17 -0300

    01a09 : CRUD

--- lessons/module01/index.js
+++ lessons/module01/index.js
@@ -2,16 +2,48 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
 // Query params = ?teste=1
 // Route params = /user/1
 // Request body = { "name": "Diego". "email": "diego@rocketseat.com"}
 
-const user = ['Diego', 'Robson', 'Victor'];
+// CRUD -  Create, Read, Update, Delete
+
+const users = ['Diego', 'Robson', 'Victor'];
+
+server.get('/users', (req, res) => {
+  return res.json(users);
+});
 
 server.get('/users/:index', (req, res) => {
-  const nome = req.query.nome;
   const { index } = req.params;
   return res.json(users[index]);
+});
+
+server.post('/users', (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  res.json(users);
+});
+
+server.put('/users/:index', (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+
+  return res.json(users);
+});
+
+server.delete('/users/:index', (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
+
+  return res.send();
 });
 
 server.listen(3000);
