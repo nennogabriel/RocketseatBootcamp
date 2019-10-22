93eb7945add5fe4806907eb4e1750c2f08302bc5
Pedro Nascimento <nennogabriel@gmail.com>
2019-10-22 08:33:38 -0300

    adonis api 07 Resetando e-mail

--- bonus/adonis_api/app/Controllers/Http/ForgotPasswordController.js
+++ bonus/adonis_api/app/Controllers/Http/ForgotPasswordController.js
@@ -1,11 +1,12 @@
 'use strict'
 
+const moment = require('moment')
 const crypto = require('crypto')
 const User = use('App/Models/User')
 const Mail = use('Mail')
 
 class ForgotPasswordController {
-  async store({ request, response }) {
+  async store ({ request, response }) {
     try {
       const email = request.input('email')
       const user = await User.findByOrFail('email', email)
@@ -33,6 +34,33 @@ class ForgotPasswordController {
       return response.status(err.status).send({
         error: { message: 'Algo não deu certo, esse email existe? ', err }
       })
+    }
+  }
+
+  async update ({ request, response }) {
+    try {
+      const { token, password } = request.all()
+
+      const user = await User.findByOrFail('token', token)
+
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at)
+      if (tokenExpired) {
+        return response.status(401).send({
+          error: { message: 'O token de recuperação está expirado' }
+        })
+      }
+
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+
+      user.save()
+    } catch (err) {
+      return response
+        .status(err.status)
+        .send({ error: { message: 'Algo deu errado ao resetar a sua senha' } })
     }
   }
 }
