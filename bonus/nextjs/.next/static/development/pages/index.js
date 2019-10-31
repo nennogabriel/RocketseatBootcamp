d8fd97b9ad1b4d0857ae0fb5396aece08ec278a4
Pedro Nascimento <nennogabriel@gmail.com>
2019-10-31 13:47:08 -0300

    nextjs 01 introducao

---
+++ bonus/nextjs/.next/static/development/pages/index.js
@@ -0,0 +1,90 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static/development/pages/index.js"],{
+
+/***/ "../../node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Fnennogabriel%2FDesktop%2Fbootcamp%2FRocketseatBootcamp%2Fbonus%2Fnextjs%2Fpages%2Findex.js!./":
+/*!************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** /Users/nennogabriel/Desktop/bootcamp/RocketseatBootcamp/node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Fnennogabriel%2FDesktop%2Fbootcamp%2FRocketseatBootcamp%2Fbonus%2Fnextjs%2Fpages%2Findex.js ***!
+  \************************************************************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/", function() {
+      var mod = __webpack_require__(/*! ./pages/index.js */ "./pages/index.js")
+      if(true) {
+        module.hot.accept(/*! ./pages/index.js */ "./pages/index.js", function() {
+          if(!next.router.components["/"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/index.js */ "./pages/index.js")
+          next.router.update("/", updatedPage)
+        })
+      }
+      return mod
+    }]);
+  
+
+/***/ }),
+
+/***/ "../../node_modules/react/index.js":
+/*!***********************************************************************************************!*\
+  !*** delegated ../../node_modules/react/index.js from dll-reference dll_127aad9e0812b57664d7 ***!
+  \***********************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_127aad9e0812b57664d7 */ "dll-reference dll_127aad9e0812b57664d7"))("../../node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "../../node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/nennogabriel/Desktop/bootcamp/RocketseatBootcamp/bonus/nextjs/pages/index.js";
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+var Home = function Home() {
+  return __jsx("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    },
+    __self: this
+  }, "Hello Wolrd");
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Home);
+
+/***/ }),
+
+/***/ 0:
+/*!**********************************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2F&absolutePagePath=%2FUsers%2Fnennogabriel%2FDesktop%2Fbootcamp%2FRocketseatBootcamp%2Fbonus%2Fnextjs%2Fpages%2Findex.js ***!
+  \**********************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2F&absolutePagePath=%2FUsers%2Fnennogabriel%2FDesktop%2Fbootcamp%2FRocketseatBootcamp%2Fbonus%2Fnextjs%2Fpages%2Findex.js! */"../../node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Fnennogabriel%2FDesktop%2Fbootcamp%2FRocketseatBootcamp%2Fbonus%2Fnextjs%2Fpages%2Findex.js!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_127aad9e0812b57664d7":
+/*!*******************************************!*\
+  !*** external "dll_127aad9e0812b57664d7" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_127aad9e0812b57664d7;
+
+/***/ })
+
+},[[0,"static/runtime/webpack.js"]]]);
+//# sourceMappingURL=index.js.map
